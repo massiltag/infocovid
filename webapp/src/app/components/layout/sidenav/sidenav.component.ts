6f931986ddac394abfb8c1f182d8b9b95a97b5c1
Massil Taguemout <massiltag@icloud.com>
2021-04-01 12:39:30 +0200

    set sidenav to push when display is xs

--- webapp/src/app/components/layout/sidenav/sidenav.component.ts
+++ webapp/src/app/components/layout/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {RoutePathEnum} from '../../../enums/route-path.enum';
 import {AuthService} from '../../../services/auth/auth.service';
 
@@ -11,10 +11,26 @@ export class SidenavComponent implements OnInit {
   @Input() opened: boolean;
   routePath = RoutePathEnum;
 
+  public screenWidth: any;
+  public screenHeight: any;
+
+
   constructor(public auth: AuthService) {}
 
   ngOnInit(): void {
     this.opened = true;
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event): void {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+
+  isXs(): boolean {
+    return this.screenHeight < 850 && this.screenWidth < 400;
   }
 
   toggle(): void {
