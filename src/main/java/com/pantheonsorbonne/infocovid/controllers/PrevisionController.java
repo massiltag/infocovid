8023044b2147657542ca3e0f827876bbee8b2dbb
William DAI <williamdai1999@hotmail.fr>
2021-04-05 21:59:47 +0200

    Add @CrossOrigin

--- src/main/java/com/pantheonsorbonne/infocovid/controllers/PrevisionController.java
+++ src/main/java/com/pantheonsorbonne/infocovid/controllers/PrevisionController.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import static com.pantheonsorbonne.infocovid.controllers.BaseController.BASE_URL;
 
 @Slf4j
 @Controller
+@CrossOrigin
 @RequiredArgsConstructor
 public class PrevisionController {
 
