b0065bcd01bc4d3e360a6e21150aad2279d3f639
sarah-meziane <sarahmez1999@gmail.com>
2021-04-08 21:48:39 +0200

    ajout commentaires

--- src/main/java/com/pantheonsorbonne/infocovid/services/impl/PrevisionServiceImpl.java
+++ src/main/java/com/pantheonsorbonne/infocovid/services/impl/PrevisionServiceImpl.java
@@ -22,61 +22,38 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PrevisionServiceImpl implements PrevisionService {
 
-    private final MetricsService metricsService;
+	private final MetricsService metricsService;
 
-    PrevisionConfinement p;
+	PrevisionConfinement p;
 
-    RegressionLineaire mmc;
+	RegressionLineaire mmc;
 
-    @Override
-    public Prevision getPrevision() {
-        List<Metrics> metrics = metricsService.getForRange(LocalDate.now().minusDays(30), LocalDate.now().minusDays(2));
+	@Override
+	public Prevision getPrevision() {
+		List<Metrics> metrics = metricsService.getForRange(LocalDate.now().minusDays(30), LocalDate.now().minusDays(2));
 
-        List<Integer> nbCas = metrics
-                .stream()
-                .map(m -> m.getRecap().getConf())
-                .collect(Collectors.toList());
+		List<Integer> nbCas = metrics.stream().map(m -> m.getRecap().getConf()).collect(Collectors.toList());
 
-        List<Integer> nbHosp = metrics
-                .stream()
-                .map(m -> m.getRecap().getHosp())
-                .collect(Collectors.toList());
+		List<Integer> nbHosp = metrics.stream().map(m -> m.getRecap().getHosp()).collect(Collectors.toList());
 
-        List<Integer> nbDc = metrics
-                .stream()
-                .map(m -> m.getRecap().getEsms_dc() + m.getRecap().getDchosp())
-                .collect(Collectors.toList());
+		List<Integer> nbDc = metrics.stream().map(m -> m.getRecap().getEsms_dc() + m.getRecap().getDchosp())
+				.collect(Collectors.toList());
 
-        p = PrevisionConfinement
-                .builder()
-                .nbCas(nbCas)
-                .nbHospitalisations(nbHosp)
-                .nbDeces(nbDc)
-                .build();
+		p = PrevisionConfinement.builder().nbCas(nbCas).nbHospitalisations(nbHosp).nbDeces(nbDc).build();
 
-        return p.previsionConfinement();
-    }
+		return p.previsionConfinement();
+	}
 
+	@Override
+	public PrevisionImmunite getRegressionLineaireImmunite() {
+		List<Metrics> metrics = metricsService.getForRange(LocalDate.now().minusDays(30), LocalDate.now().minusDays(2));
 
-    @Override  
-    public PrevisionImmunite getRegressionLineaireImmunite() {
-        List<Metrics> metrics = metricsService.getForRange(LocalDate.now().minusDays(30), LocalDate.now().minusDays(2));
- //       List<Metrics> metrics = metricsService.getForRange(LocalDate.now().minusDays(30), LocalDate.now().minusDays(2));
-  
+		List<Integer> nbVaccins = metrics.stream().map(m -> m.getVaccineStats().getN_cum_dose1())
+				.collect(Collectors.toList());
 
-        List<Integer> nbVaccins = metrics
-                .stream()
-                .map(m -> m.getVaccineStats().getN_cum_dose1())
-                .collect(Collectors.toList());
+		mmc = RegressionLineaire.builder().nbVaccinationsList(nbVaccins).jourList(new ArrayList<Integer>()).build();
 
-        mmc = RegressionLineaire
-                .builder()
-                .nbVaccinationsList(nbVaccins)
-                .jourList(new ArrayList<Integer>())
-                .build();
-        System.out.println(nbVaccins);
-       
-        return mmc.previsionImmunite();
-    }
+		return mmc.previsionImmunite();
+	}
 
 }
